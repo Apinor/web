f3dad44c401470e1b2f47ec9cfe94b5b7f2bc888
Konk32 <131777454+Konk32@users.noreply.github.com>
2025-02-06 11:22:42 +0100

    Live update for items on stickers page when deleting

--- admin/public/panel/stickers/script.js
+++ admin/public/panel/stickers/script.js
@@ -49,15 +49,17 @@ function deleteItem(id){
     body: JSON.stringify({ id: id }),
     credentials: "include",
   })
-    .then((response) => {
-      if (response.ok) {
-        alert("Stickers deleted successfully!");
-      } else {
-        response.json().then((error) => {
-          alert(`Error: ${error.error}`);
-        });
+  .then(response => response.json())
+  .then(data => {
+    if (data.success) {
+      const productElement = document.getElementById(`stickersID-${id}`);
+      if (productElement) {
+        productElement.remove();
       }
-    })
+    } else {
+      console.error('Deletion failed:', data.error);
+    }
+  })
     .catch((error) => {
       alert("Failed to delete stickers: " + error.message);
     });
