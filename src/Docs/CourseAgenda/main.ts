e59702079885291836f121b7c116714d9e311dca
Konk32 <131777454+Konk32@users.noreply.github.com>
2025-02-08 16:23:03 +0100

    fix

--- src/Docs/CourseAgenda/main.ts
+++ src/Docs/CourseAgenda/main.ts
@@ -2,6 +2,6 @@
 // main.ts
 import { renderFileToString } from "https://deno.land/x/dejs@0.10.3/mod.ts";
 
-const html = await renderFileToString("./views/index.ejs", {});
+const html = await renderFileToString("./index.ejs", {});
 
 Deno.serve({ port: 8000 }, () => new Response(html, { headers: { "content-type": "text/html" } }));
