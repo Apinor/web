1259e00a441d0403da407393bedecaf0f7924fae
Konk32 <131777454+Konk32@users.noreply.github.com>
2025-02-09 19:12:27 +0100

    "Reactive EJS title"

--- src/Docs/CourseAgenda/main.ts
+++ src/Docs/CourseAgenda/main.ts
@@ -2,6 +2,6 @@
 // main.ts
 import { renderFileToString } from "https://deno.land/x/dejs@0.10.3/mod.ts";
 
-const html = await renderFileToString("./index.ejs", {});
+const html = await renderFileToString("./index.ejs", {title: "EJS WORKS"});
 
 Deno.serve({ port: 8000 }, () => new Response(html, { headers: { "content-type": "text/html" } }));
